b8a8b0b646f28a565013656e631cc6d751562a32
Tommaso Bratto <tbratto22@gmail.com>
2015-01-29 08:33:00 +0000

    ex. 2: added index number to names

--- directory.rb
+++ directory.rb
@@ -4,8 +4,8 @@ puts "-------------"
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
